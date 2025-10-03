7a24920287ac48e7155794cd6d075c5bad2a0275
yowoodzm <127jeda@gmail.com>
2025-10-02 21:33:02 -0400

    ts is not loading rn bruh moment

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+letBearStage;
+
+function preload(){
+   bearStage = loadimage("C:\Users\ganal\Downloads\bearStage.gif");
+}
+
+
+function setup() 
+{
+	createCanvas(400, 400);
+}
+
+function draw()
+{
+       background(220,200,200);
+    }
